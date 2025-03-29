305f00acdf1c1b2e9cc941a462afac55a2b3d50b
vettyuwu <hyp3erek@gmail.com>
2025-03-29 12:27:16 +0100

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -57,7 +57,7 @@ imageInput.addEventListener('change', (event) => {
     fetch('	https://api.imgur.com/3/image' ,{
         method: 'POST',
         headers: {
-            'Authorization': 'Client-ID 4ecc257cbb25ccc'
+            'Authorization': 'Client-ID 8110e2818fe5def'
         },
         body: data
     })
